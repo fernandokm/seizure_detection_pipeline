f42543145d88583add34e68c7115d64a8f86933d
Viennot Louis <louis.viennot@student-cs.fr>
2022-03-24 11:33:04 +0100

    bases pour grafana

---
+++ env.sh
@@ -0,0 +1,22 @@
+# InfluxDB env vars
+export INFLUXDB_USERNAME=admin
+export INFLUXDB_PASSWORD=wdkjsdk8hcjbw
+export INFLUXDB_DATABASE=visualizations
+
+# Grafana env vars
+export GRAFANA_USERNAME=admin
+export GRAFANA_PASSWORD=admin
+export POSTGRES_DATABASE_GRAFANA=grafana
+
+# PostgreSQL env vars
+export POSTGRES_HOST_URL=postgres
+export POSTGRES_DATABASE=postgres
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=wdkjsdk8hcjbw
+
+
+# Ports
+export POSTGRES_PORT=5432
+export INFLUXDB_PORT=8086
+export GRAFANA_PORT=3000
+
