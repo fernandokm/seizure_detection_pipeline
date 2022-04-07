da69b2349234c2bf4ec9df3508803f53114de885
Viennot Louis <louis.viennot@student-cs.fr>
2022-04-07 17:06:58 +0200

    module initialization.postgresql

--- src/initialization/generate_test_data.py
+++ src/initialization/generate_test_data.py
@@ -6,7 +6,7 @@ import pyedflib
 from datetime import datetime, timedelta
 import rx
 import influxDB
-
+import postgresql
 
 POSTGRES_HOST_URL = os.environ.get("POSTGRES_LOCALHOST")
 POSTGRES_DATABASE = os.environ.get("POSTGRES_DATABASE")
@@ -23,38 +23,20 @@ EDF_SOURCES = {
     3281: "data/tuh/dev/01_tcp_ar/002/00003281/00003281_s001_t001.edf"
 }
 
-DATA = {
-    "test_table": [
-        {
-            "id": 1,
-            "name": "test_name",
-        },
-        {
-            "id": 2,
-            "name": "test_name2",
-        }
-    ]
-}
+CSV_SOURCES = {}
 
-def generate_postgresql_data(data):
+def generate_postgresql_data(csv_files):
     """
-    Generate data in postgresql
-    data: input data {
-        table_name: [{column1: value1, column2: value2}, {column1: value1, column2: value2},...]
-    }
+    Generate data in postgresql from csv files
     """
-    conn = open_postgresql_session()
-    try:
-        with conn.cursor() as curs:
-            for table,content in data.items():
-                generate_table(curs, table, content)
-                conn.commit()
-    except Exception as e:
-        conn.rollback()
-        print(e)
-    finally:
-        conn.close()
-
+    data = postgresql.get_data_from_csv_dict(csv_files)
+    postgresql.push_postgresql_data(
+        data=data,
+        host=POSTGRES_HOST_URL,
+        database=POSTGRES_DATABASE,
+        username=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+    )
 
 def generate_influxdb_data(edf_files, T_sample=1):
     """
@@ -65,53 +47,6 @@ def generate_influxdb_data(edf_files, T_sample=1):
     for patient, file in edf_files.items():
         influxDB.push_ecg_to_influxdb(file, patient=patient, host=IDB_HOST, port=IDB_PORT, username=IDB_USERNAME, password=IDB_PASSWORD, database=IDB_DATABASE)
 
-def open_postgresql_session():
-    """
-    Open postgresql session
-    """
-    return psycopg2.connect(host=POSTGRES_HOST_URL, database=POSTGRES_DATABASE, user=POSTGRES_USER, password=POSTGRES_PASSWORD, port=5432)
-
-def generate_table(cursor, table, content):
-    """
-    Create table in postgresql & fill it
-    cursor: cursor
-    table: table name
-    content: table content
-    """
-    cursor.execute(f"DROP TABLE IF EXISTS {table}")
-    first_row: list = content[0]
-    columns_sql = ", ".join((map(lambda column: f"{column} varchar(255)" if type(column) == str else f"{column} integer", first_row.keys())))
-    cursor.execute(f"CREATE TABLE {table} ({columns_sql})")
-    fill_table(cursor, table, content)
-
-def fill_table(cursor, table, content):
-    """
-    Fills existing table in postgresql with content 
-    cursor: cursor
-    table: table name
-    content: table content
-    """
-    for row in content:
-        columns_sql = ", ".join((map(lambda column: f"{column}", row.keys())))
-        values_sql = ", ".join((map(lambda column: f"'{row[column]}'" if type(row[column]=="str") else f"{row[column]}", row.keys())))
-        cursor.execute(f"INSERT INTO {table} ({columns_sql}) VALUES ({values_sql})")
-
-def get_data_from_csv(filepath):
-    """
-    Not used
-    """
-    with open(filepath, "r") as csv_file:
-        reader = csv.DictReader(csv_file)
-        return list(reader)
-
-def get_data_from_csv_dict(csv_files):
-    """
-    Not used
-    """
-    data = {}
-    for table, filepath in csv_files.items():
-        data[table] = get_data_from_csv(filepath)
-    return data
 
 def log(msg):
     """
@@ -120,9 +55,7 @@ def log(msg):
     print(f"{datetime.now().strftime('%H:%M:%S')}: {msg}")
 
 if __name__ == "__main__":
-    # data = get_data_from_csv("file.csv")
-    # data = get_data_from_csv_dict({"test_table": "file.csv"})
-    # generate_postgresql_data(DATA)
     log("Starting execution")
+    generate_postgresql_data(CSV_SOURCES)
     generate_influxdb_data(EDF_SOURCES)
     log("Execution finished")
