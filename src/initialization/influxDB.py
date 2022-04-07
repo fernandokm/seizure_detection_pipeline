2ef7c5fe2a57e72f9c6700e99bf6a6e808b3a4b6
Viennot Louis <louis.viennot@student-cs.fr>
2022-04-07 22:00:17 +0200

    push features to influxDB

--- src/initialization/influxDB.py
+++ src/initialization/influxDB.py
@@ -1,9 +1,12 @@
 from typing import Iterator, Any, Union
 
+import csv
 from influxdb_client import InfluxDBClient, WriteOptions
 from pyedflib import EdfReader
 import rx
 import sys
+import datetime
+from math import floor
 
 sys.path.append(".")
 from src.infrastructure.edf_loader import EdfLoader
@@ -42,7 +45,7 @@ def from_edf(filepath: str, channel: int, mesurement: Union[str, None] = None, t
         tagslist = ",".join([f"{escape(k)}={escape(v)}" for k, v in tags.items()] + [f"channel={escape(r.getSignalLabels()[channel])}"])
         tagslist = f",{tagslist}" if tagslist else ""
         for val in r.readSignal(channel):
-            yield f"{mesurement}{tagslist} {escape(field)}={val} {int(time*1e9)}"
+            yield f"{mesurement}{tagslist} {escape(field)}={escapevalue(val)} {to_nanoseconds(time)}"
             # yield Point(f"{mesurement}").tag("channel",f"{channel}").field(f"Tension (uV)", val).time(time)
             time += T_sample_real
 
@@ -56,3 +59,60 @@ def push_ecg_to_influxdb(ecg_filepath: str, **kwargs) -> None:
 def escape(s: str) -> str:
     s = str(s)
     return str(s).replace(" ", "\\ ").replace(",", "\\,").replace("=", "\\=")
+
+def escapevalue(value: Any) -> str:
+    """
+    Escape value if it can't be interpreted as a finite number
+    """
+    try:
+        # drop unreadable values
+        if abs(float(value)) in [float("inf"), float("nan")]:
+            return ""
+        return str(value)
+    except:
+        if len(str(value)) > 0:
+            return f"\"{value}\""
+        else:
+            return ""
+
+def push_csv_features_to_influxdb(csv_filepath: str, **kwargs) -> None:
+    data = rx.from_iterable(from_csv(csv_filepath, measurement="features", tags={"patient": kwargs.get("patient") or csv_filepath.split("/")[-1].split(".")[0]}, time_column="timestamp"))
+    push_influxdb_data(data, **kwargs)
+
+def from_csv(csv_filepath: str, measurement: Union[None, str] = None, tags: dict = {}, time_column: str = "timestamp", **kwargs) -> Iterator[str]:
+    """
+    Get points from csv file
+    csv_filepath: filepath
+    """
+    with open(csv_filepath, "r") as f:
+        reader = csv.DictReader(f)
+        measurement = escape(measurement or csv_filepath)
+        tagslist = ",".join([f"{escape(k)}={escape(v)}" for k, v in tags.items()])
+        tagslist = f",{tagslist}" if tagslist else ""
+        for row in reader:
+            time = row[time_column]
+            for k, v in row.items():
+                value = escapevalue(v)
+                if k == time_column or value == "":
+                    continue
+                yield f"{measurement}{tagslist} {escape(k)}={value} {to_nanoseconds(time)}"
+
+def to_nanoseconds(date_time: Any, mult_to_seconds=1):
+    """
+    TODO: comment better
+    Convert date_time to nanoseconds
+    date_time: can be str (int seconds timestamp), str (DD-MM-YYYY HH:MM:SS.m), datetime, int or float (in seconds)
+    """
+    mult = 1e9*mult_to_seconds
+    if type(date_time) == str:
+        try:
+            return int(float(date_time)*mult)
+        except:
+            return int(datetime.datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S.%f").timestamp()*1e9)
+    elif type(date_time) == int:
+        return int(date_time*mult)
+    elif type(date_time) == float:
+        return int(date_time*mult)
+    elif type(date_time) == datetime.datetime:
+        return int(date_time.timestamp()*1e9)
+    raise TypeError("Invalid date_time type: {}".format(type(date_time)))
