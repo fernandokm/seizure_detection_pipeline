6621b68c5bf6add5d06f7af6b2274b7e09c52af4
Viennot Louis <louis.viennot@student-cs.fr>
2022-04-07 16:54:45 +0200

    structure changes, added aura src folder, initialization.influxDB module

--- src/initialization/generate_test_data.py
+++ src/initialization/generate_test_data.py
@@ -1,9 +1,12 @@
 import psycopg2
 import os
 import csv
-from influxdb_client import InfluxDBClient, Point
+from influxdb_client import InfluxDBClient, Point, WriteOptions
 import pyedflib
 from datetime import datetime, timedelta
+import rx
+import influxDB
+
 
 POSTGRES_HOST_URL = os.environ.get("POSTGRES_LOCALHOST")
 POSTGRES_DATABASE = os.environ.get("POSTGRES_DATABASE")
@@ -17,7 +20,7 @@ IDB_PASSWORD = os.environ.get("INFLUXDB_PASSWORD")
 IDB_DATABASE = os.environ.get("INFLUXDB_DATABASE")
 
 EDF_SOURCES = {
-    9578: "data/tuh/dev/01_tcp_ar/002/00009578/00009578_s002_t001.edf"
+    3281: "data/tuh/dev/01_tcp_ar/002/00003281/00003281_s001_t001.edf"
 }
 
 DATA = {
@@ -59,31 +62,8 @@ def generate_influxdb_data(edf_files, T_sample=1):
     edf_files: input edf files []
     T_sample: later
     """
-    url = f"http://{IDB_HOST}:{IDB_PORT}"
-    bucket = f'{IDB_DATABASE}/autogen' # ??
     for patient, file in edf_files.items():
-        points = get_points_from_edf(file)
-        with InfluxDBClient(url=url, token=f"{IDB_USERNAME}:{IDB_PASSWORD}", org="-") as client:
-            with client.write_api() as write_api:
-                for p in points:
-                    write_api.write(bucket, record=p.to_line_protocol())
-
-def get_points_from_edf(edf_file):
-    """
-    get influxdb points from edf file
-    edf_file: filepath
-    """
-    r = pyedflib.EdfReader(edf_file)
-    points = []
-    channel_i = 0
-    for channel in r.getSignalLabels():
-        T_sample_real = 1/r.getSampleFrequency(channel_i)
-        time = r.getStartdatetime()
-        for val in r.readSignal(channel_i):
-            points.append(Point(f"{edf_file}").tag("channel",f"{channel}").field(f"Tension (uV)", val).time(time))
-            time += timedelta(seconds=T_sample_real)
-        channel_i += 1
-    return points
+        influxDB.push_ecg_to_influxdb(file, patient=patient, host=IDB_HOST, port=IDB_PORT, username=IDB_USERNAME, password=IDB_PASSWORD, database=IDB_DATABASE)
 
 def open_postgresql_session():
     """
@@ -133,9 +113,16 @@ def get_data_from_csv_dict(csv_files):
         data[table] = get_data_from_csv(filepath)
     return data
 
+def log(msg):
+    """
+    Log message
+    """
+    print(f"{datetime.now().strftime('%H:%M:%S')}: {msg}")
 
 if __name__ == "__main__":
     # data = get_data_from_csv("file.csv")
     # data = get_data_from_csv_dict({"test_table": "file.csv"})
     # generate_postgresql_data(DATA)
+    log("Starting execution")
     generate_influxdb_data(EDF_SOURCES)
+    log("Execution finished")
