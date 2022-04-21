8d4896c99c4ea549ba482977aa330a275203a278
Chloé Muller <chloe.muller@student-cs.fr>
2022-04-21 09:42:45 +0200

    added plotly dashboards + upload postgresql data

--- src/initialization/generate_test_data.py
+++ src/initialization/generate_test_data.py
@@ -14,13 +14,9 @@ IDB_USERNAME = os.environ.get("INFLUXDB_USERNAME")
 IDB_PASSWORD = os.environ.get("INFLUXDB_PASSWORD")
 IDB_DATABASE = os.environ.get("INFLUXDB_DATABASE")
 
-EDF_SOURCES = {
-    3281: "data/tuh/dev/01_tcp_ar/002/00003281/00003281_s001_t001.edf"
-}
+EDF_SOURCES = {3281: "data/tuh/dev/01_tcp_ar/002/00003281/00003281_s001_t001.edf"}
 
-CSV_SOURCES = {
-    3281: "output/cons_00003281_s001_t001_t001.csv"
-}
+CSV_SOURCES = {3281: "output/cons_00003281_s001_t001_t001.csv"}
 
 
 def generate_postgresql_data(csv_files: dict = {}):
@@ -38,16 +34,30 @@ def generate_postgresql_data(csv_files: dict = {}):
 
 
 def generate_influxdb_data(edf_files: dict = {}, csv_files: dict = {}):
-    """ 
+    """
     Generate data in influxdb from edf files
     edf_files: input edf files []
     """
     for patient, file in edf_files.items():
-        influxDB.push_ecg_to_influxdb(file, patient=patient, host=IDB_HOST, port=IDB_PORT,
-                                      username=IDB_USERNAME, password=IDB_PASSWORD, database=IDB_DATABASE)
+        influxDB.push_ecg_to_influxdb(
+            file,
+            patient=patient,
+            host=IDB_HOST,
+            port=IDB_PORT,
+            username=IDB_USERNAME,
+            password=IDB_PASSWORD,
+            database=IDB_DATABASE,
+        )
     for patient, file in csv_files.items():
         influxDB.push_csv_features_to_influxdb(
-            file, patient=patient, host=IDB_HOST, port=IDB_PORT, username=IDB_USERNAME, password=IDB_PASSWORD, database=IDB_DATABASE)
+            file,
+            patient=patient,
+            host=IDB_HOST,
+            port=IDB_PORT,
+            username=IDB_USERNAME,
+            password=IDB_PASSWORD,
+            database=IDB_DATABASE,
+        )
 
 
 def log(msg):
--- src/initialization/postgresql.py
+++ src/initialization/postgresql.py
@@ -1,13 +1,11 @@
+from cmath import isinf
 import psycopg2
 import csv
+from datetime import datetime
+
 
 def push_postgresql_data(
-    data: dict,
-    host: str,
-    database: str,
-    username: str,
-    password: str,
-    port: int = 5432
+    data: dict, host: str, database: str, username: str, password: str, port: int = 5432
 ):
     """
     Push data to postgresql
@@ -15,10 +13,12 @@ def push_postgresql_data(
         table_name: [{column1: value1, column2: value2}, {column1: value1, column2: value2},...]
     }
     """
-    conn = psycopg2.connect(host=host, database=database, user=username, password=password, port=port)
+    conn = psycopg2.connect(
+        host=host, database=database, user=username, password=password, port=port
+    )
     try:
         with conn.cursor() as curs:
-            for table,content in data.items():
+            for table, content in data.items():
                 generate_table(curs, table, content)
                 conn.commit()
     except Exception as e:
@@ -26,6 +26,7 @@ def push_postgresql_data(
         print(e)
     finally:
         conn.close()
+
 
 def generate_table(cursor, table, content):
     """
@@ -36,21 +37,44 @@ def generate_table(cursor, table, content):
     """
     cursor.execute(f"DROP TABLE IF EXISTS {table}")
     first_row: list = content[0]
-    columns_sql = ", ".join((map(lambda column: f"{column} varchar(255)" if type(column) == str else f"{column} integer", first_row.keys())))
+
+    def type_maping(item):
+        column, value = item
+        if type(value) == str:
+            return f"{column} varchar(255)"
+        elif isinstance(value, datetime):
+            return f"{column} timestamp"
+        elif isinstance(value, float):
+            return f"{column} real"
+        else:
+            return f"{column} integer"
+
+    columns_sql = ", ".join((map(type_maping, first_row.items())))
     cursor.execute(f"CREATE TABLE {table} ({columns_sql})")
     fill_table(cursor, table, content)
 
+
 def fill_table(cursor, table, content):
     """
-    Fills existing table in postgresql with content 
+    Fills existing table in postgresql with content
     cursor: cursor
     table: table name
     content: table content
     """
     for row in content:
         columns_sql = ", ".join((map(lambda column: f"{column}", row.keys())))
-        values_sql = ", ".join((map(lambda column: f"'{row[column]}'" if type(row[column]=="str") else f"{row[column]}", row.keys())))
+        values_sql = ", ".join(
+            (
+                map(
+                    lambda column: f"'{row[column]}'"
+                    if type(row[column] == "str")
+                    else f"{row[column]}",
+                    row.keys(),
+                )
+            )
+        )
         cursor.execute(f"INSERT INTO {table} ({columns_sql}) VALUES ({values_sql})")
+
 
 def get_data_from_csv(filepath):
     """
@@ -58,6 +82,7 @@ def get_data_from_csv(filepath):
     """
     with open(filepath, "r") as csv_file:
         return csv.DictReader(csv_file)
+
 
 def get_data_from_csv_dict(csv_files):
     """
@@ -67,3 +92,49 @@ def get_data_from_csv_dict(csv_files):
     for table, filepath in csv_files.items():
         data[table] = get_data_from_csv(filepath)
     return data
+
+
+if __name__ == "__main__":
+
+    def string_to_date(string):
+        return datetime.strptime(string, "%Y-%m-%d %H:%M:%S")
+
+    # data = {"my_test_table_2": [{"time": string_to_date("2022-01-18 06:03:41"), "letter": "A", "number": 2.0}, {"time": string_to_date("2022-01-18 07:03:41"), "letter": "B", "number": -4.3}, {
+    #     "time": string_to_date("2022-01-18 08:03:41"), "letter": "C", "number": 5.1}, {"time": string_to_date("2022-01-18 09:03:41"), "letter": "D", "number": 0.0}, {"time": string_to_date("2022-01-18 10:03:41"), "letter": "E", "number": 1.2}]}
+    # push_postgresql_data(data, "localhost", "grafana",
+    #                      "postgres", "wdkjsdk8hcjbw")
+    data = {
+        "my_test_table_2": [
+            {
+                "time": string_to_date("2022-01-18 06:03:41"),
+                "letter": "sdnn > 45.06",
+                "number": 0.047,
+            },
+            {
+                "time": string_to_date("2022-01-18 07:03:41"),
+                "letter": "38.08 < rmssd <= 47.95",
+                "number": -0.0125,
+            },
+            {
+                "time": string_to_date("2022-01-18 08:03:41"),
+                "letter": "median_nni > 710.00",
+                "number": 0.011,
+            },
+            {
+                "time": string_to_date("2022-01-18 09:03:41"),
+                "letter": "0.05 < cvsd <= 0.06",
+                "number": -0.009,
+            },
+            {
+                "time": string_to_date("2022-01-18 10:03:41"),
+                "letter": "1.00 < nni_50 <= 3.00",
+                "number": -0.006,
+            },
+            {
+                "time": string_to_date("2022-01-18 10:03:41"),
+                "letter": "lf > 808.47",
+                "number": 0.008,
+            },
+        ]
+    }
+    push_postgresql_data(data, "localhost", "grafana", "postgres", "wdkjsdk8hcjbw")
