126e132176cfbad1c0b563228094df5e27110bfc
adcollin <101322033+adcollin@users.noreply.github.com>
2022-05-19 10:20:53 +0200

    Update shap_pipeline.py
    Add lime interpretation

--- src/usecase/shap_pipeline.py
+++ src/usecase/shap_pipeline.py
@@ -5,6 +5,7 @@ import pickle
 
 import pandas as pd
 import shap
+from lime import lime_tabular
 
 
 sys.path.append('.')
@@ -15,12 +16,24 @@ from src.usecase.compute_hrvanalysis_features import FEATURES_KEY_TO_INDEX
 def shap_pipeline(model, db):
     model_columns = list(FEATURES_KEY_TO_INDEX.keys())
     shap_columns = [0]*len(model_columns)
+    lime_columns = [0]*len(model_columns)
+    lime_table = np.zeros(len(model_columns), db.shape(0])
     for i in range(len(model_columns)):
         shap_columns[i] = "shap_values_"+model_columns[i]
+        lime_columns[i] = "lime_values_"+model_columns[i]
     explainer_patient = shap.TreeExplainer(model)
+    explainer_lime = lime_tabular.LimeTabularExplainer(training_data=db[model_colums].values, 
+                                              mode='classification', 
+                                              class_names=['no seizure', 'seizure'],
+                                              feature_names=model_columns)
     shap_values_graf = explainer_patient.shap_values(db[model_columns])
     db[shap_columns] = shap_values_graf[1]
-
+    for i in range(db.shape[0]):
+        sample = db.iloc[[i]].values
+        exp = explainer.explain_instance(sample[0], model.predict_proba, labels=(0, 1), num_features=len(model_columns))
+        for j in range(len(model_columns):
+            lime_table[i, j] = exp.as_list()[j][1]
+     db[lime_columns] = lime_table
 
 def shap_pipeline_cli(model_name, db_name, output_file):
     db = pd.read_csv(db_name)
